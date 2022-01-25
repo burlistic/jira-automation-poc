9e6216472d4a6af9829447858106f78f69a971ba
Thomas Cleary <thomasclearydev@gmail.com>
2022-01-25 14:09:52 +0800

    add script to deploy bicep infra to racwa vs sub in azure (not complete) + add python vscode settings

---
+++ infra/az-cli-deploy/deploy.py
@@ -0,0 +1,99 @@
+"""
+Script to deploy the main.bicep file to my RACWA Visual Studio Subscription
+using the Azure CLI
+"""
+
+import re
+import subprocess
+
+from enum import IntEnum
+
+
+class SubscriptionListIndexes(IntEnum):
+    SUB_NAME   = 0
+    SUB_ID     = 1
+    STATE      = 2
+    IS_DEFAULT = 3
+
+
+def cmd_return_output(command):
+    """ Run the command (string) and return the output (string) """
+    return subprocess.run(command.split(), check=True, text=True, capture_output=True).stdout
+
+
+def newlines_to_space(string):
+    """ Replace all newline characters in string with a space character """
+    return string.replace('\n', ' ')
+
+
+def remove_table_headings(table_row_list, num_headings):
+    """ 
+    Remove the headings from the list of table rows
+    """
+    return table_row_list[num_headings:]
+
+
+def join_sub_name(subscription):
+    subscription = subscription.split('AzureCloud')
+    subscription[0] = '-'.join(subscription[0].split())
+
+    subscription = ' '.join(subscription).split()
+    return subscription
+
+def reduce_internal_whitespace(string_list):
+    """ Reduce the internal whitespace of each string in stringlist to one space """
+    return [re.sub(r'\s+', ' ', string) for string in string_list]
+
+
+def get_subscription_dict(subscription):
+    """ return a tuple (sub_name, info_dict) """
+    sub_info = {
+        'subscription_id' : subscription[SubscriptionListIndexes.SUB_ID],
+        'state' : subscription[SubscriptionListIndexes.STATE]
+    }
+    return (subscription[SubscriptionListIndexes.SUB_NAME], sub_info)
+
+
+def subscription_list_to_dict(subscription_list):
+    """ Return list of subscription strings in dict form """
+    subscriptions = {}
+    for subscription in subscription_list:
+        subscription = join_sub_name(subscription)
+        name, info_dict = get_subscription_dict(subscription)
+        subscriptions[name] = info_dict
+
+    return subscriptions
+
+
+def get_azure_subscriptions_logged_in():
+    """ return a list of accounts that are logged into az cli """
+    NUM_TABLE_HEADINGS = 2
+
+    account_table_rows = cmd_return_output('az account list -o table').rstrip().split('\n')
+    subscriptions = remove_table_headings(account_table_rows, NUM_TABLE_HEADINGS)
+    return subscription_list_to_dict(reduce_internal_whitespace(subscriptions))
+
+
+
+def main():
+    """ Deploys main.bicep to RACWA VS Sub assuming Azure CLI is already logged in """
+
+    RACWA_SUB_NAME = 'Visual-Studio-Professional-Subscription'
+
+    subscriptions = get_azure_subscriptions_logged_in()
+    racwa_sub_logged_in = RACWA_SUB_NAME in subscriptions.keys()
+
+    if not racwa_sub_logged_in:
+        # login
+        subscriptions = get_azure_subscriptions_logged_in()
+
+    racwa_sub_enabled = subscriptions[RACWA_SUB_NAME]["state"].lower() == 'enabled'
+    if not racwa_sub_enabled:
+        # enable the subscription
+        subscriptions = get_azure_subscriptions_logged_in()
+
+    # deploy bicep
+        
+
+if __name__ == "__main__":
+    main()
