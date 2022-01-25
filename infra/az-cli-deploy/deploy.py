98df04375e5388b774c966d92a0e17db67cb9669
Thomas Cleary <thomasclearydev@gmail.com>
2022-01-25 14:39:47 +0800

    add login reattempts and checking

--- infra/az-cli-deploy/deploy.py
+++ infra/az-cli-deploy/deploy.py
@@ -3,6 +3,7 @@ Script to deploy the main.bicep file to my RACWA Visual Studio Subscription
 using the Azure CLI
 """
 
+from configparser import RawConfigParser
 from get_login_table_helpers import *
 from shell_command_helpers import *
 
@@ -14,17 +15,50 @@ def main():
 
     subscriptions = get_azure_subscriptions_logged_in()
     racwa_sub_logged_in = racwa_sub_name in subscriptions
+    try_again = True
 
-    if not racwa_sub_logged_in:
-        subscriptions = get_azure_subscriptions_logged_in()
+    if racwa_sub_logged_in:
+        print("- RACWA VisualStudio Subscription already logged in.")
+    else:
+        while not racwa_sub_logged_in and try_again:
+            print(f"- {racwa_sub_name} not found in logged in subscriptions.")
+            print("- Attempting login via browser (2FA).")
 
-    else: print("sub found")
+            azure_login()
+            subscriptions = get_azure_subscriptions_logged_in()
+
+            racwa_sub_logged_in = racwa_sub_name in subscriptions
+
+            if racwa_sub_name not in subscriptions:
+
+                valid_input = False
+                while not valid_input:
+                    login_again = input("- Login Unsuccessful. Try Again? (y/n): ").lower()
+
+                    if login_again not in ['y', 'n']:
+                        continue
+                    elif login_again == 'n':
+                        try_again = False
+
+                    valid_input = True
+
+                continue
+                    
+        if not racwa_sub_logged_in:
+            return
+        
+        print("- Login successful.")
+
+
     racwa_sub_enabled = subscriptions[racwa_sub_name]["state"].lower() == 'enabled'
-    if not racwa_sub_enabled:
+
+    if racwa_sub_enabled:
+        print("RACWA VisualStudio Subscription already enabled.")
+    else:
         # enable the subscription
         subscriptions = get_azure_subscriptions_logged_in()
 
-    else: print("sub enabled")
+    
 
     # deploy bicep
 
--- infra/az-cli-deploy/shell_command_helpers.py
+++ infra/az-cli-deploy/shell_command_helpers.py
@@ -5,3 +5,7 @@ import subprocess
 def cmd_return_output(command):
     """ Run the command (string) and return the output (string) """
     return subprocess.run(command.split(), check=True, text=True, capture_output=True).stdout
+
+def azure_login():
+    """ run az login command """
+    return cmd_return_output('az login')
