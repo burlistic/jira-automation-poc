9b1c2b32b8329ddd159d0731ce81bc2fe28d186d
Thomas Cleary <thomasclearydev@gmail.com>
2022-01-25 15:24:01 +0800

    fix get_default_sub_name and add other azure commands to helper file

--- infra/az-cli-deploy/deploy-main-bicep.py
+++ infra/az-cli-deploy/deploy-main-bicep.py
@@ -3,9 +3,11 @@ Script to deploy the main.bicep file to my RACWA Visual Studio Subscription
 using the Azure CLI
 """
 
+from asyncio.proactor_events import _ProactorBaseWritePipeTransport
 from configparser import RawConfigParser
 from get_login_table_helpers import *
 from shell_command_helpers import *
+from set_subscription_helpers import *
 
 
 def main():
@@ -13,6 +15,7 @@ def main():
 
     racwa_sub_name = 'Visual-Studio-Professional-Subscription'
 
+    # Login to Azure #########################################################
     subscriptions = get_azure_subscriptions_logged_in()
     racwa_sub_logged_in = racwa_sub_name in subscriptions
     try_again = True
@@ -49,8 +52,10 @@ def main():
         
         print("- Login successful.")
 
-
+    # Set default subscription ###############################################
+    old_default_sub_name = get_default_sub_name(subscriptions) 
     racwa_sub_enabled = subscriptions[racwa_sub_name]["is_default"].lower() == 'true'
+
     if racwa_sub_enabled:
         print("- RACWA VisualStudio Subscription already enabled.")
     else:
@@ -64,8 +69,23 @@ def main():
         else:
             print("- RACWA VisualStudio Subsciption enabled successfully.")
 
-    # deploy bicep
+    # Deploy azure infra with Bicep files ####################################
+    location = 'australiaeast' # think this is needed because its a sub level deployment
+    bicep_file_path = './infra/main.bicep' # using root of repo as that's where running script from
 
+    print(f"- Deploying {bicep_file_path} to {racwa_sub_name}...")
+    deploy_main_bicep_file(location, bicep_file_path)
+
+    # Logout racwa account ###################################################
+    azure_logout()
+
+    if old_default_sub_name is not None and old_default_sub_name != racwa_sub_name:
+        azure_account_set(subscriptions[old_default_sub_name]["subscription_id"])
+        print(f"- Subscription default reset to {old_default_sub_name}")
+
+
+    print("- Subscriptions currently logged in to AZ CLI:")
+    print(list_logged_in_subscriptions())
 
 if __name__ == "__main__":
     main()
--- infra/az-cli-deploy/shell_command_helpers.py
+++ infra/az-cli-deploy/shell_command_helpers.py
@@ -10,6 +10,20 @@ def azure_login():
     """ run az login command """
     return cmd_return_output('az login')
 
+def azure_logout():
+    """ run az logout command """
+    return cmd_return_output('az logout')
+
 def azure_account_set(subscription_id):
     """ Set the current subscription to use to subscription_id """
     return cmd_return_output(f'az account set --subscription {subscription_id}')
+
+def list_logged_in_subscriptions():
+    """ return the output form listing subscriptions with az cli """
+    return cmd_return_output('az account list -o table')
+
+def deploy_main_bicep_file(location, path_to_file):
+    """ return the output from deploying the main bicep file """
+    return cmd_return_output(
+        f'az deployment sub create --location {location} --template-file {path_to_file}'
+    )
