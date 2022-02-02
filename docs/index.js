087e88faca3057a0c56015c01df9c07c62431202
Thomas Cleary <thomasclearydev@gmail.com>
2022-02-02 05:15:26 +0000

    #82: add simple frontend to consume function app #major

--- docs/index.js
+++ docs/index.js
@@ -2,28 +2,79 @@ window.onload = () => {
     init();
 }
 
+
 function init() {
-    getDev();
+    addGetBtnOnClick();
+}
+
+function addGetBtnOnClick() {
+    const getButton = document.getElementById("get-btn");
+    getButton.onclick = getDev;
 }
 
 function getDev() {
     const xhttp = new XMLHttpRequest();
 
-    xhttp.onload = () => {
-        if (xhttp.status == 200) {
-            const responseText = JSON.parse(xhttp.responseText);
-            updateTitle(responseText["message"]);
+    xhttp.onreadystatechange = () => {
+        if(xhttp.readyState == XMLHttpRequest.DONE) {
+            if (xhttp.status == 200) {
+                const responseText = JSON.parse(xhttp.responseText);
+                setTimeout(() => {
+                    updateMessageText(responseText["message"]);
+                    updateGetBtnText();
+                }, 2000);
+            }
+            else {
+                console.log(xhttp.status);
+            }
         }
         else {
-            console.log(xhttp.status);
+            updateMessageLoading();
+            updateGetBtnLoading();
         }
+
     }
 
-    xhttp.open("GET", "http://localhost:7071/api/environment");
+    xhttp.open("GET", "https://fa-prd-cicd-practice.azurewebsites.net/api/environment");
     xhttp.send();
 }
 
-function updateTitle(message) {
-    const titleHeading = document.getElementById("title");
-    titleHeading.innerHTML = message;
+function updateMessageText(message) {
+    const titleMessage = document.getElementById("get-msg");
+    titleMessage.innerHTML = message;
+}
+
+function updateMessageLoading() {
+    const titleMessage = document.getElementById("get-msg");
+
+    const spinnerDiv = document.createElement("div");
+    spinnerDiv.classList.add("spinner-border", "text-primary");
+
+    const spinnerSpan = document.createElement("span");
+    spinnerSpan.classList.add("sr-only");
+
+    spinnerDiv.appendChild(spinnerSpan);
+
+    titleMessage.innerHTML = '';
+    titleMessage.appendChild(spinnerDiv);
+}
+
+function updateGetBtnText() {
+    const getButton = document.getElementById("get-btn");
+    getButton.innerHTML = "GET";
+}
+
+function updateGetBtnLoading() {
+    const getButton = document.getElementById("get-btn");
+
+    const spinnerDiv = document.createElement("div");
+    spinnerDiv.classList.add("spinner-grow", "spinner-grow-sm");
+
+    const spinnerSpan = document.createElement("span");
+    spinnerSpan.classList.add("sr-only");
+
+    spinnerDiv.appendChild(spinnerSpan);
+
+    getButton.innerHTML = '';
+    getButton.appendChild(spinnerDiv);
 }
